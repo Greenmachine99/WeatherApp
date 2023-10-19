6ef3967fcce3ca215f831d725df729884101da2d
Greenmachine <sam.o.groen99@gmail.com>
2023-10-19 19:24:17 +0200

    Test

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from sqlalchemy import create_engine, Integer, Column, String
+import os 
+from dotenv import load_dotenv
 
 # Creating Flask App
 
