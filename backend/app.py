a2c1f69dd9618f427f9468a1f355c32d966aaba2
Greenmachine <sam.o.groen99@gmail.com>
2023-10-19 20:09:19 +0200

    Start Flask API

--- backend/app.py
+++ backend/app.py
@@ -5,5 +5,26 @@ from sqlalchemy import create_engine, Integer, Column, String
 import os 
 from dotenv import load_dotenv
 
+# Loading Environment Variables
+load_dotenv()
+
 # Creating Flask App
+app = Flask(__name__)
+# Connecting to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=True)
+# Creating API
+api = Api(app)
+
+# Create Resource Class
+class gps(Resource):
+    def get (self):
+        # Querying Database
+        result = db.execute("SELECT * FROM gps")
+        # Returning Data
+        return {'data': [dict(row) for row in result]}
+
+# Adding Resource to API
+api.add_resource(gps, '/gps')
+
 
