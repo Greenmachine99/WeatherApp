652a5d4e01cbfb837e6640b3ce02812614f2e5a0
Greenmachine <sam.o.groen99@gmail.com>
2023-10-22 12:17:50 +0200

    Created Basic Fundament

---
+++ frontend/src/features/GenInfo/genInfo.js
@@ -0,0 +1,14 @@
+// Import Necessary Dependencies
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+// Import CSS Module
+import './genInfo.css';
+
+// Import Slice Dependencies
+
+
+// Build Component
+export function GenInfo() {
+    
+}
---
+++ frontend/src/features/GenInfo/genInfoSlice.js
@@ -0,0 +1,21 @@
+// Import Dependencies
+import { createSlice } from '@reduxjs/toolkit';
+
+// Declare Initial State
+const initialState = {
+    // Declare State Variables
+    temperature: null,
+    humidity: null,
+    rain: null,
+    status: 'idle',
+}
+
+// Fetch Backend Data
+
+// Build Slice
+
+// Export Slice Actions
+
+// Export Slice Selectors
+
+// Export Slice Reducer
