1dae00f802437f52ae6278e9091a16939ef1e93c
Greenmachine <sam.o.groen99@gmail.com>
2023-11-07 16:39:31 +0100

    OpenWeatherAPI Connection Base

--- frontend/src/features/GenInfo/genInfo.js
+++ frontend/src/features/GenInfo/genInfo.js
@@ -1,25 +1,39 @@
 // Import Necessary Dependencies
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 // Import CSS Module
 import './genInfo.css';
 
 // Import Slice Dependencies
+import { loadWeather, tempFeelslikeSelector, tempSelector, windSpeedSelector, cloudinessSelector, rainSelector, isLoadingSelector, hasErrorSelector } from './genInfoSlice';
+import { startPositionSelector } from '../WeatherInfo/MapInfo/mapInfoSlice';
 
 // Build Component
 export function GenInfo() {
+    const dispatch = useDispatch();
 
     // Declare State Variables
-    const temperature = 20;
-    const humidity = 90;
-    const precipitation = 10;
+    const isLoading = useSelector(isLoadingSelector);
+    const hasError = useSelector(hasErrorSelector);
+
+    const temperature = useSelector(tempSelector);
+    const temperatureFeelsLike = useSelector(tempFeelslikeSelector);
+    const precipitation = useSelector(rainSelector);
+    const windSpeed = useSelector(windSpeedSelector);
+
+    const [lat, lon] = useSelector(startPositionSelector);
+
+    // Load Weather Data
+    useEffect(() => {
+        dispatch(loadWeather({lat ,lon}));
+      }, [dispatch, lat, lon]);
 
     return (
         <div className = 'weather-header'>
             <h1> Current Weather Info </h1>
             <h1> {temperature}°C  </h1>
-            <h1> {humidity}% </h1>
+            <h1> {temperatureFeelsLike}°C </h1>
             <h1> {precipitation}mm </h1>
         </div> 
     )
--- frontend/src/features/GenInfo/genInfoSlice.js
+++ frontend/src/features/GenInfo/genInfoSlice.js
@@ -20,6 +20,7 @@ const initialState = {
     tempFeelslike: 0,
     windSpeed: 0,
     cloudiness: 0,
+    rain: 0,
 
     isLoading: false,
     hasError: false
@@ -47,6 +48,7 @@ const options = {
             state.tempFeelslike = action.payload.main.feels_like;
             state.windSpeed =  action.payload.wind.speed;
             state.cloudiness = action.payload.clouds.all;
+            state.rain = action.payload.rain['1h'];
         })
         .addCase(loadWeather.rejected, state => {
             state.isLoading = false;
@@ -64,6 +66,7 @@ export const tempSelector = state => state.weather.temp;
 export const tempFeelslikeSelector = state => state.weather.tempFeelslike;
 export const windSpeedSelector = state => state.weather.windSpeed;
 export const cloudinessSelector = state => state.weather.cloudiness;
+export const rainSelector = state => state.weather.rain;
 
 export const isLoadingSelector = state => state.weather.isLoading;
 export const hasErrorSelector = state => state.weather.hasError;
