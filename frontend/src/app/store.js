f7979aa427591f4241907e2c67906b1bbafd8398
Greenmachine <sam.o.groen99@gmail.com>
2023-11-08 18:59:47 +0100

    Removed Small Bug

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,11 +3,9 @@ import { configureStore } from '@reduxjs/toolkit';
 // Import Individual Reducers
 import mapReducer from '../features/WeatherInfo/MapInfo/mapInfoSlice.js';
 import genInfoReducer from '../features/GenInfo/genInfoSlice.js';
-import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     map: mapReducer,
     weather: genInfoReducer
   },
